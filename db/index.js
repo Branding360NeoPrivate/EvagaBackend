98a52d57e5e8afc21be0a467d7a6b93cf6cd13c9
arman8808 <armanal3066@gmail.com>
2024-12-18 13:00:48 +0530

    commit

--- db/index.js
+++ db/index.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
