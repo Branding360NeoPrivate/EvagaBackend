bae53527e9525564f205fa5f4d5ac817b676fd07
arman8808 <armanal3066@gmail.com>
2024-12-25 13:34:21 +0530

    commit

--- controllers/vendor.service.form.controller.js
+++ controllers/vendor.service.form.controller.js
@@ -285,7 +285,7 @@ const addVenderService = async (req, res) => {
 
         transformedValues[value.key] = value.items;
       });
-      service.menu.forEach((value) => {
+      service?.menu?.forEach((value) => {
         transMenuValues[value.key] = value.items;
       });
 
