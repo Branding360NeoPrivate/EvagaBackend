788980c066cf3514e9cb2b24b8ceff0001c2aaaf
arman8808 <armanal3066@gmail.com>
2025-02-12 17:27:49 +0530

    added dotenv

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -8,7 +8,9 @@ import {
   CopyObjectCommand,
   DeleteObjectCommand,
 } from "@aws-sdk/client-s3";
+import dotenv from "dotenv";
 
+dotenv.config();
 const s3 = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
