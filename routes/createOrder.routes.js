860241d38037d3eb41224f8ec9ea85ede7366c3f
arman8808 <armanal3066@gmail.com>
2025-02-26 15:27:43 +0530

    commented the verify jwt

--- routes/createOrder.routes.js
+++ routes/createOrder.routes.js
@@ -1,11 +1,22 @@
 import { Router } from "express";
-import { createOrder, updateOrder } from "../controllers/createOrder.controller.js";
+import {
+  createOrder,
+  updateOrder,
+} from "../controllers/createOrder.controller.js";
 import verifyJwt from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/create-order/:userId/:numberOfParts").post(verifyJwt(["user"]),upload().none(), createOrder);
-router.route("/update-order").post(verifyJwt(["user"]),upload().none(), updateOrder);
+router.route("/create-order/:userId/:numberOfParts").post(
+  // verifyJwt(["user"]),
+  upload().none(),
+  createOrder
+);
+router.route("/update-order").post(
+  // verifyJwt(["user"]),
+  upload().none(),
+  updateOrder
+);
 
 export default router;
