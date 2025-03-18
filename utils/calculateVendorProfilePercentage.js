9b1f0c526270c4b4ed460629ed838a0d7cd93ef0
arman8808 <armanal3066@gmail.com>
2025-03-18 12:01:45 +0530

    change in logic of vendor profile calculation

--- utils/calculateVendorProfilePercentage.js
+++ utils/calculateVendorProfilePercentage.js
@@ -1,26 +1,26 @@
 const calculateProfileCompletion = (vendor) => {
-  const totalFields = 12 + 9; // 13 base fields + 9 document fields
+  const totalFields = 11;
   let filledFields = 0;
-
-  // Check base fields
   if (vendor.venderID) filledFields++;
   if (vendor.email) filledFields++;
   if (vendor.phoneNumber) filledFields++;
-  if (vendor.password) filledFields++;
   if (vendor.name) filledFields++;
-  if (vendor.documents && Array.isArray(vendor.documents)) {
-    // Count filled documents
-    filledFields += vendor.documents.filter((doc) => doc).length; // Assuming `doc` is considered filled if it's truthy
-  }
-  if (vendor.alternatePhoneNumber) filledFields++;
+  if (vendor.businessDetails) filledFields++;
   if (vendor.bio) filledFields++;
-  if (vendor.location) filledFields++;
-  if (vendor.areaOfInterest) filledFields++;
-  if (vendor.yearOfExperience) filledFields++;
   if (vendor.profilePicture) filledFields++;
   if (vendor.bankDetails) filledFields++;
-
-  // Calculate percentage
+  if (vendor.businessDetails) {
+    const {
+      adharVerificationStatus,
+      gstVerificationStatus,
+      panVerificationStatus,
+    } = vendor.businessDetails;
+    filledFields += [
+      adharVerificationStatus,
+      gstVerificationStatus,
+      panVerificationStatus,
+    ].filter((status) => status === "Verified").length;
+  }
   const completionPercentage = Math.round((filledFields / totalFields) * 100);
   return completionPercentage;
 };
