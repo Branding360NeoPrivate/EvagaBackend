32821b31aa260e4b58216458bd2dddcf1e2cf158
arman8808 <armanal3066@gmail.com>
2025-05-08 22:14:37 +0530

    added booking cta

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ import blog from "./routes/blog.routes.js";
 import newsletter from "./routes/newsLetter.routes.js";
 import logerror from "./routes/errorLog.routes.js";
 import review from "./routes/review.routes.js";
+import bookingCTA from "./routes/bookingCTA.routes.js";
 import helmet from "helmet";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -160,6 +161,7 @@ app.use("/api/v1/blog", blog);
 app.use("/api/v1/review", review);
 app.use("/api/v1/newsletter", newsletter);
 app.use("/api/v1/logerror", logerror);
+app.use("/api/v1/bookingCTA", bookingCTA);
 app.get("/", async (req, res) => {
   res.status(200).json("Server Is Live");
 });
---
+++ controllers/bookingCTA.controller.js
@@ -0,0 +1,103 @@
+import bookingCTA from "../modals/bookingCTA.js";
+import sendEmailWithTemplete from "../utils/mailer.js";
+
+// Create new booking
+export const createBooking = async (req, res) => {
+  try {
+    const booking = await bookingCTA.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: booking,
+    });
+    await sendEmailWithTemplete(
+      "thankyou",
+      booking?.email,
+      "Thank You for Reaching Out to Evaga!"
+    );
+    await sendEmailWithTemplete(
+      "adminCtaNotification",
+      "armanal3066@gmail.com",
+      "New Booking Form Submission - Evaga",
+      {
+        name: booking?.name,
+        email: booking?.email,
+        phone: booking?.phone,
+        eventType: booking?.eventType,
+        preferredDate: booking?.preferredDate,
+      }
+    );
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+// Get all bookings (for admin)
+export const getBookings = async (req, res) => {
+  try {
+    const { page = 1, limit = 10, search } = req.query;
+
+    let query = {};
+    console.log(query, page, req.query);
+
+    // Add search functionality for phone and email
+    if (search) {
+      query = {
+        $or: [
+          { phone: { $regex: search, $options: "i" } },
+          { email: { $regex: search, $options: "i" } },
+        ],
+      };
+    }
+
+    const bookings = await bookingCTA
+      .find(query)
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .exec();
+
+    const count = await bookingCTA.countDocuments(query);
+
+    res.status(200).json({
+      success: true,
+      data: bookings,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+// Mark as read (admin)
+export const markAsRead = async (req, res) => {
+  try {
+    const booking = await BookingCTA.findByIdAndUpdate(
+      req.params.id,
+      { isRead: true },
+      { new: true, runValidators: true }
+    );
+
+    if (!booking) {
+      return res.status(404).json({
+        success: false,
+        error: "Booking not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: booking,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: "Server Error",
+    });
+  }
+};
